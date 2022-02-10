9a661fd5a08a576f798f3e70759e6fbd551221ae
Prince Jan Kumi <janprince002@gmail.com>
2022-02-10 13:24:52 +0000

    modified constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 // color constants
 const Color kPrimary = Color.fromRGBO(255, 250, 240, 1);
 const Color kSecondary = Color.fromRGBO(247, 240, 231, 1);
-const Color kBlue = Color.fromRGBO(19, 85, 238, 1);
+// const Color kBlue = Color.fromRGBO(19, 85, 238, 1);
+const Color kBlue = Colors.blueGrey;
